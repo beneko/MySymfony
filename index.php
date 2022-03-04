e7b5923f80608fa2aae35a9b05d6734da0214b09
beneko <behzadnekouei@gmail.com>
2022-03-04 16:47:35 +0100

    create a request object and print name in the index.php page

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-$name = isset($_GET['name']) ? $_GET['name'] : 'World';
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__. '/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+// var_dump($request);
+// die();
+
+$name = $request->query->get('name', 'World');
+
+// $name = isset($_GET['name']) ? $_GET['name'] : 'World';
 
 // header('Content-Type: text/html; charset=utf-8');
 
